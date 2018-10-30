96fc207f54f21b41168f81185c7a513b71c16f38
Ralf Wiechers <ralf@cross-media-cloud.de>
2018-10-30 15:40:15 +0100

    add chromedriver as offline resource

--- wdio.conf.js
+++ wdio.conf.js
@@ -137,6 +137,7 @@ exports.config = {
 	// your test setup with almost no effort. Unlike plugins, they don't add new
 	// commands. Instead, they hook themselves up into the test process.
 	services: [
+		'chromedriver',
 		'selenium-standalone',
 		'visual-regression',
 		'screenshots-cleanup',
